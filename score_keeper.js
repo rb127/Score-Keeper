6035f0de859d8f6580b0be983494e599b31e5873
Rohit Bansal <rohitbansal0602@yahoo.com>
2019-08-27 08:53:43 +0530

    made updating total efficient, game now works properly till winning score, and also refactored a bit.

--- score_keeper.js
+++ score_keeper.js
@@ -12,19 +12,26 @@ var p2Disp = document.getElementById("p2Disp");
 
 // var result = p1Score + " to " + p2Score;
 // h1.innerHTML = result;
-setInterval(function(){
-  var input = document.getElementById("total");
-  var value = input.value;
-  if(value !== ""){
-    defaultVal = value;
-  var totalResult = "Playing to: " + value;
-  var finalVal = document.getElementById("finalVal");
-  finalVal.innerHTML = totalResult;
-  }
-},500);
-
-
-console.log(p1Score + " to " + p2Score);
+// setInterval(function(){
+//   var input = document.getElementById("total");
+//   var value = input.value;
+//   if(value !== ""){
+//     defaultVal = value;
+//   var totalResult = "Playing to: " + value;
+//   var finalVal = document.getElementById("finalVal");
+//   finalVal.innerHTML = totalResult;
+//   }
+// },500);
+var input = document.querySelector("input");
+input.addEventListener("change", function(){
+    //console.log("input event changed");
+    defaultVal = Number(input.value);
+    var str = "Playing to: " + defaultVal;
+    var finalVal = document.getElementById("finalVal");
+    finalVal.innerHTML = str;
+    resetGame();
+});
+//console.log(p1Score + " to " + p2Score);
 // while(p1Score !== defaultVal || p2Score != defaultVal){
 p1.addEventListener("click", function(){
   if(!gameOver){
@@ -37,7 +44,7 @@ p1.addEventListener("click", function(){
 }
   // h1.innerHTML = updateScore();
   p1Disp.innerHTML = p1Score;
-  console.log(p1Score + " to " + p2Score);
+//  console.log(p1Score + " to " + p2Score);
 });
 
 p2.addEventListener("click", function(){
@@ -50,23 +57,26 @@ p2.addEventListener("click", function(){
 }
   // h1.innerHTML = updateScore();
   p2Disp.innerHTML = p2Score;
-
-  console.log(p1Score + " to " + p2Score);
+//  console.log(p1Score + " to " + p2Score);
 });
 
-function updateScore(){
- var result = p1Score + " to " + p2Score;
- return result;
-}
+// function updateScore(){
+//  var result = p1Score + " to " + p2Score;
+//  return result;
+// }
 // }
 
 reset.addEventListener("click", function(){
-    p1Score = 0;
-    p2Score = 0;
-    // h1.innerHTML = updateScore();
-    p1Disp.innerHTML = p1Score;
-    p2Disp.innerHTML = p2Score;
-    p1Disp.classList.remove("winnerDisp");
-    p2Disp.classList.remove("winnerDisp");
-    gameOver = false;
+    resetGame();
 });
+
+function resetGame(){
+  p1Score = 0;
+  p2Score = 0;
+  // h1.innerHTML = updateScore();
+  p1Disp.innerHTML = p1Score;
+  p2Disp.innerHTML = p2Score;
+  p1Disp.classList.remove("winnerDisp");
+  p2Disp.classList.remove("winnerDisp");
+  gameOver = false;
+}
